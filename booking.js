8ce047d45e208cfb272264e8795191e2d244fb8d
Christian de Barrio Arribas <christiandebarrio@gmail.com>
2016-07-06 13:59:32 +0200

    toogle class to selected items and disabled book button

--- booking.js
+++ booking.js
@@ -1,3 +1,19 @@
 (function($) {
   // do stuff
-})(jQuery)
+})(jQuery);
+
+function selectSlot (slot) {
+
+  var slot_selected = slot.currentTarget;
+
+  $(slot_selected).toggleClass("selected");
+
+  if($(".list-group-item.selected").size() > 1) {
+    $("#bookButton").removeClass("disabled");
+  } else {
+    $("#bookButton").addClass("disabled");
+  }
+}
+
+
+$(".list-group").on("click", ".list-group-item", selectSlot);
